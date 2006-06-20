f69e162919f528f36ddfcb9f2545f92dcf2c9ece
Jimmy Wennlund <jimmy@wennlund.nu>
2006-06-20 15:30:29 +0000

    Fixes.

--- initfiles/default.s
+++ initfiles/default.s
@@ -3,6 +3,6 @@
 setup()
 {
     iregister runlevel
-	iset need = "clock getty initial udev hostname net/lo keymaps"
+	iset need = "clock getty initial udev hostname net/lo keymaps acpid dbus hald speedstep"
 	idone
 }
