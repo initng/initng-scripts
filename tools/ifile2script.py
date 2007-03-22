8fdc556f68ab8f7e68f8b863fef6279e985d8df8
Ismael Luceno <ismael@initng.org>
2007-03-22 02:17:12 +0000

    Two small fixes for ifile2script.py

--- tools/ifile2script.py
+++ tools/ifile2script.py
@@ -3,7 +3,7 @@
 """
 Usage:
 	ifile2script.py path/to/<scriptname>.ii
-	
+
 	The converted script will be placed in CWD.
 """
 
@@ -70,28 +70,28 @@ for i in file_list:
 		iset_list.append(_service)
 		service_and_daemon_list.append(_service)
 		file_mode = "1"
-	
+
 	elif i.startswith("daemon"):
 		_service = i.split()[1].strip("{").strip()
 		daemon_list.append(_service)
 		iset_list.append(_service)
 		service_and_daemon_list.append(_service)
 		file_mode = "1"
-	
+
 	elif i.startswith("virtual"):
 		_service = i.split()[1].strip("{").strip()
 		virtual_list.append(_service)
 		iset_list.append(_service)
 		service_and_daemon_list.append(_service)
 		file_mode = "1"
-		
+
 	elif file_mode == "1":
 		i = i.strip(" \t")
 		if i.startswith("script"):
 			file_mode = "2"
 			iexec_list.append(_service)
 			iexec_list.append("%" + i.split()[1].strip() + "%")
-			
+
 		elif i.startswith("need") or i.startswith("use") or i.startswith("also_start") or \
 				i.startswith("pid_file"):
 			_str = i.split("=")[-1].strip(" ;")
@@ -103,24 +103,27 @@ for i in file_list:
 				_sel = "also_start = "
 			elif  i.startswith("pid_file"):
 				_sel = "pid_file = "
-			
+
 			if iset_list_mode != elsed_mode and elsed_mode != 0:
 				for i in range(-1 - iset_list_mode, -1 - len(ifd_list), -1):
 					iset_list.append(ifd_list[i])
 				iset_list_mode = elsed_mode
 			iset_list.append(_sel + '"' + _str + '"')
-		
+
 		elif i.startswith("env"):
 			_str = i.split(" ", 1)[1].strip(" ;")
 			if i.split()[0].strip() == "env_file":
 				_str = _str.strip(" ;=")
 				_str = "source " + _str
+			else:
+				_str = _str.split("=")[0] + '="'+ _str.split("=")[1] + '"'
+			
 			if env_list_mode != elsed_mode and elsed_mode != 0:
 				for i in range(-1 - env_list_mode, -1 - len(ifd_list), -1):
 					env_list.append(ifd_list[i])
 				env_list_mode = elsed_mode
 			env_list.append(_str)
-				
+
 		elif i.startswith("exec"):
 			_list = i.split(" ", 1)[-1].strip(" ;").split("=")
 			_sel = "exec "
@@ -129,18 +132,18 @@ for i in file_list:
 					iset_list.append(ifd_list[i])
 				iset_list_mode = elsed_mode
 			iset_list.append(_sel + _list[0].strip() + ' = "' + _list[1].strip() + '"')
-		
+
 		elif i.startswith("#ifd"):
 			ifd_mode = i.strip()
 			elsed_mode = i.strip()
 			ifd_list.insert(0, i.strip())
 			elsed_mode = len(ifd_list)
-				
+
 		elif i.startswith("#elsed"):
 			elsed_mode = i.strip()
 			ifd_list.insert(0, i.strip())
 			elsed_mode = len(ifd_list)
-				
+
 		elif i.startswith("#endd"):
 			for g in range(len(ifd_list)):
 				if ifd_list[g].startswith("#ifd"):
@@ -159,31 +162,31 @@ for i in file_list:
 									ifd_mode = h
 									break
 					break
-				
+
 			if env_list_mode != 0:
 				env_list.append("#endd")
 				env_list_mode = elsed_mode
 			if iset_list_mode != 0:
 				iset_list.append("#endd")
 				iset_list_mode = elsed_mode
-				
+
 		elif i != "" and i != "}" and not i.startswith("#"):
 			if "#" in i:
 				_int = i.find("#")
 				i = i[:_int]
 			iset_list.append(i.strip(" ;"))
-	
+
 	elif file_mode == "2":
 		_str = i.strip(" \t")
 		if _str == "};":
 			file_mode = "1"
 		else:
 			iexec_list.append(i)
-			
+
 	file_int = file_int + i_int
 
 
-new_file = open(os.getcwd() + "/" + scriptname.rstrip("i") + "ss", "w")
+new_file = open(os.getcwd() + "/" + scriptname.rstrip("i") + "s", "w")
 
 _str = """# NAME:
 # DESCRIPTION:
@@ -197,7 +200,7 @@ for i in range(3):
 		if i == 0:
 			new_file.write(_str)
 			break
-		
+
 new_file.write("\n")
 
 for i in env_list:
@@ -210,13 +213,13 @@ new_file.write("setup()\n{\n")
 
 for i in virtual_list:
 	new_file.write("\tiregister -s %s virtual\n" %(i))
-	
+
 for i in service_list:
 	new_file.write("\tiregister -s %s service\n" %(i))
 
 for i in daemon_list:
 	new_file.write("\tiregister -s %s daemon\n" %(i))
-	
+
 new_file.write("\n")
 
 for i in iset_list:
@@ -236,7 +239,7 @@ for i in iexec_list:
 		new_file.write("\tiexec -s %s start = %s_start\n" %(_service, os.path.basename(_service)))
 	elif i == "%stop%":
 		new_file.write("\tiexec -s %s stop = %s_stop\n" %(_service, os.path.basename(_service)))
-		
+
 new_file.write("\n")
 
 for i in service_and_daemon_list:
@@ -263,12 +266,12 @@ for i in iexec_list:
 		_mode = "2"
 	else:
 		new_file.write(i + "\n")
-		
+
 if _mode != "0":
 	new_file.write("}\n\n")
-	
+
 new_file.close()
-print scriptname.rstrip("i") + "ss" + " is ready.\n"
+print scriptname.rstrip("i") + "s" + " is ready.\n"
 
 
 
