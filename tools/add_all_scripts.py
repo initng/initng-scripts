1639d18bf41b34d7fd5782f4a8320f89646fb953
Jens Persson <xerxes2@gmail.com>
2007-03-23 20:32:17 +0000

    added microseconds to add_all_scripts.py

--- tools/add_all_scripts.py
+++ tools/add_all_scripts.py
@@ -34,7 +34,7 @@ clock_0 = datetime.datetime.now()
 scriptcount_int = 0
 
 for _dir in dir_list:
-	print "Converting %s dicectory...\n" %(_dir)
+	print "Converting %s directory ...\n" %(_dir)
 	os.chdir(scripts_path + "/" + _dir)
 	ifiles_list = os.listdir(ifiles_path + "/" + _dir)
 	for i in ifiles_list:
@@ -44,25 +44,8 @@ for _dir in dir_list:
 			scriptcount_int = scriptcount_int + 1
 	print " ... done.\n"
 
-
 clock_1 = datetime.datetime.now()
 timediff = clock_1 - clock_0
-seconds = timediff.seconds
-hours = 0
-minutes = 0
-if seconds > 3599:
-	hours = seconds / 3600
-	seconds = seconds - (hours * 3600)
-if seconds > 59:
-	minutes = seconds / 60
-	seconds = seconds - (minutes * 60)
-hours = str(hours)
-minutes = str(minutes)
-if len(minutes) == 1:
-	minutes = "0" + minutes
-seconds = str(seconds)
-if len(seconds) == 1:
-	seconds = "0" + seconds
-time_str = hours + ":" + minutes + ":" + seconds
-print str(scriptcount_int) + " scripts converted in %s.\n" %(time_str)
+time_str = str(timediff.seconds) + "." + str(timediff.microseconds)
+print str(scriptcount_int) + " scripts converted in %s seconds.\n" %(time_str)
 
