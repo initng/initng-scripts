ab606d7d1ae0f2f2b08190c7c119b96177b8a6c3
Jens Persson <xerxes2@gmail.com>
2007-03-24 19:08:02 +0000

    updated cmake for initng-scripts so it actually builds

--- cmake/InitngMacros.cmake
+++ cmake/InitngMacros.cmake
@@ -1,23 +1,29 @@
-MACRO(PROCESS_SFILES _s_FILES)
+MACRO(PROCESS_SFILES _service_FILES)
 	# reset the variable
-	SET(${_s_FILES})
-
+	SET(${_service_FILES})
+	SET(_s_IGNORE)
+	
 	FOREACH(_current_FILE ${ARGN})
-		GET_FILENAME_COMPONENT(_tmp_FILE ${_current_FILE} ABSOLUTE)
-		GET_FILENAME_COMPONENT(_abs_PATH ${_tmp_FILE} PATH)
-		GET_FILENAME_COMPONENT(_basename ${_tmp_FILE} NAME_WE)
-		SET(_s_FILE ${CMAKE_CURRENT_BINARY_DIR}/${_basename})
-		
-		ADD_CUSTOM_COMMAND(OUTPUT ${_s_FILE}
-			COMMAND ${CMAKE_BINARY_DIR}/tools/install_service_file
-			ARGS -i ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE} -o ${_s_FILE} > /dev/null 2>&1
-			DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE} ${CMAKE_BINARY_DIR}/tools/install_service_file)
-		
-		SET(${_s_FILES} ${${_s_FILES}} ${_s_FILE})
-			
-	ENDFOREACH(_current_FILE)
+		GET_FILENAME_COMPONENT(_basename ${_current_FILE} NAME_WE)
 
-	ADD_CUSTOM_TARGET(sfiles ALL 
-		DEPENDS ${${_s_FILES}})	
+		# process files only once
+		IF(NOT _s_IGNORE MATCHES /${_basename}\\.s;)
+			GET_FILENAME_COMPONENT(_abs_PATH ${_current_FILE} PATH)
+			SET(_service_FILE ${CMAKE_CURRENT_BINARY_DIR}/${_basename})
+
+			ADD_CUSTOM_COMMAND(OUTPUT ${_service_FILE}
+				COMMAND ${CMAKE_BINARY_DIR}/tools/install_service_file
+				ARGS -i ${_current_FILE} -o ${_service_FILE} > /dev/null 2>&1
+				DEPENDS ${_current_FILE} ${CMAKE_BINARY_DIR}/tools/install_service_file)
+
+			SET(${_service_FILES} ${${_service_FILES}} ${_service_FILE})
+
+			# add to the list of processed files
+			SET(_s_IGNORE ${_s_IGNORE} /${_basename}.s)
+		ENDIF(NOT _s_IGNORE MATCHES /${_basename}\\.s;)
+	ENDFOREACH(_current_FILE)
+	
+	ADD_CUSTOM_TARGET(sfiles ALL DEPENDS ${${_service_FILES}})
 
 ENDMACRO(PROCESS_SFILES)
+
