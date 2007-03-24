fb9dd00ba8c2fcd4bfb9152542d463c88dedd83a
Jens Persson <xerxes2@gmail.com>
2007-03-24 00:15:06 +0000

    fixed #ifd in ifile2script.py

--- tools/ifile2script.py
+++ tools/ifile2script.py
@@ -53,10 +53,16 @@ virtual_list = []
 service_and_daemon_list = []
 env_list = []
 new_file_list = []
+script_list = []
 env_list_mode = 0
+env_list_mode_1 = 0
 iexec_list_mode = 0
+iexec_list_mode_1 = 0
 iset_list_mode = 0
-ifd_mode = None
+iset_list_mode_1 = 0
+script_list_mode = 0
+script_list_mode_1 = 0
+ifd_mode = 0
 elsed_mode = 0
 ifd_list = []
 file_mode = "0"
@@ -64,7 +70,49 @@ file_int = 0
 
 for i in file_list:
 	i_int = len(i)
-	if i.startswith("service"):
+	if i.startswith("#ifd"):
+			ifd_mode = ifd_mode + 1
+			elsed_mode = i.strip()
+			ifd_list.insert(0, i.strip())
+			elsed_mode = len(ifd_list)
+
+	elif i.startswith("#elsed"):
+			elsed_mode = i.strip()
+			ifd_list.insert(0, i.strip())
+			elsed_mode = len(ifd_list)
+
+	elif i.startswith("#endd"):
+			ifd_mode = ifd_mode - 1
+			for g in range(len(ifd_list)):
+				if ifd_list[g].startswith("#ifd"):
+					ifd_list = ifd_list[g + 1:]
+					if ifd_list == []:
+						elsed_mode = 0
+					else:
+						if len(ifd_list) == 1:
+							elsed_mode = 1
+						else:
+							elsed_mode = len(ifd_list)
+					break
+
+			if env_list_mode != 0 and env_list_mode_1 == ifd_mode + 1:
+				env_list.append("#endd")
+				env_list_mode = elsed_mode
+				env_list_mode_1 = env_list_mode_1 - 1
+			if iset_list_mode != 0 and iset_list_mode_1 == ifd_mode + 1:
+				iset_list.append("#endd")
+				iset_list_mode = elsed_mode
+				iset_list_mode_1 = iset_list_mode_1 - 1
+			if iexec_list_mode != 0 and iexec_list_mode_1 == ifd_mode + 1:
+				iexec_list.append("#endd")
+				iexec_list_mode = elsed_mode
+				iexec_list_mode_1 = iexec_list_mode_1 - 1
+			if script_list_mode != 0 and script_list_mode_1 == ifd_mode + 1:
+				script_list.append("#endd")
+				script_list_mode = elsed_mode
+				script_list_mode_1 = script_list_mode_1 - 1
+	
+	elif i.startswith("service"):
 		_service = i.split()[1].strip("{").strip()
 		service_list.append(_service)
 		iset_list.append(_service)
@@ -89,9 +137,24 @@ for i in file_list:
 		i = i.strip(" \t")
 		if i.startswith("script"):
 			file_mode = "2"
+			if iexec_list_mode != elsed_mode and elsed_mode != 0:
+				for g in range(-1 - iexec_list_mode, -1 - len(ifd_list), -1):
+					iexec_list.append("%" + ifd_list[g])
+					if ifd_list[g].startswith("#ifd"):
+						iexec_list_mode_1 = iexec_list_mode_1 + 1
+				iexec_list_mode = elsed_mode
 			iexec_list.append(_service)
 			iexec_list.append("%" + i.split()[1].strip())
-
+			
+			if script_list_mode != elsed_mode and elsed_mode != 0:
+				for g in range(-1 - script_list_mode, -1 - len(ifd_list), -1):
+					script_list.append("%" + ifd_list[g])
+					if ifd_list[g].startswith("#ifd"):
+						script_list_mode_1 = script_list_mode_1 + 1
+				script_list_mode = elsed_mode
+			script_list.append(_service)
+			script_list.append("%" + i.split()[1].strip())
+			
 		elif i.startswith("need") or i.startswith("use") or i.startswith("also_start") or \
 				i.startswith("pid_file") or i.startswith("provide"):
 			_str = i.split("=")[-1].strip(" ;")
@@ -107,8 +170,10 @@ for i in file_list:
 				_sel = "provide = "
 
 			if iset_list_mode != elsed_mode and elsed_mode != 0:
-				for i in range(-1 - iset_list_mode, -1 - len(ifd_list), -1):
-					iset_list.append(ifd_list[i])
+				for g in range(-1 - iset_list_mode, -1 - len(ifd_list), -1):
+					iset_list.append(ifd_list[g])
+					if ifd_list[g].startswith("#ifd"):
+						iset_list_mode_1 = iset_list_mode_1 + 1
 				iset_list_mode = elsed_mode
 			iset_list.append(_sel + '"' + _str + '"')
 			
@@ -120,58 +185,22 @@ for i in file_list:
 				_str = _str.split("=")[0] + '="'+ _str.split("=")[1] + '"'
 		
 			if env_list_mode != elsed_mode and elsed_mode != 0:
-				for i in range(-1 - env_list_mode, -1 - len(ifd_list), -1):
-					env_list.append(ifd_list[i])
+				for g in range(-1 - env_list_mode, -1 - len(ifd_list), -1):
+					env_list.append(ifd_list[g])
+					if ifd_list[g].startswith("#ifd"):
+						env_list_mode_1 = env_list_mode_1 + 1
 				env_list_mode = elsed_mode
 			env_list.append(_str)
 
 		elif i.startswith("exec"):
 			_list = i.split(" ", 1)[-1].strip(" ;").split("=")
 			if iexec_list_mode != elsed_mode and elsed_mode != 0:
-				for i in range(-1 - iexec_list_mode, -1 - len(ifd_list), -1):
-					iexec_list.append(ifd_list[i])
+				for g in range(-1 - iexec_list_mode, -1 - len(ifd_list), -1):
+					iexec_list.append(ifd_list[g])
+					if ifd_list[g].startswith("#ifd"):
+						iexec_list_mode_1 = iexec_list_mode_1 + 1
 				iexec_list_mode = elsed_mode
 			iexec_list.append("&" + _list[0].strip() + ' = "' + _list[1].strip() + '"')
-
-		elif i.startswith("#ifd"):
-			ifd_mode = i.strip()
-			elsed_mode = i.strip()
-			ifd_list.insert(0, i.strip())
-			elsed_mode = len(ifd_list)
-
-		elif i.startswith("#elsed"):
-			elsed_mode = i.strip()
-			ifd_list.insert(0, i.strip())
-			elsed_mode = len(ifd_list)
-
-		elif i.startswith("#endd"):
-			for g in range(len(ifd_list)):
-				if ifd_list[g].startswith("#ifd"):
-					ifd_list = ifd_list[g + 1:]
-					if ifd_list == []:
-						ifd_mode = None
-						elsed_mode = 0
-					else:
-						if len(ifd_list) == 1:
-							ifd_mode = ifd_list[0]
-							elsed_mode = 1
-						else:
-							elsed_mode = len(ifd_list)
-							for h in ifd_list:
-								if h.startswith("#ifd"):
-									ifd_mode = h
-									break
-					break
-
-			if env_list_mode != 0:
-				env_list.append("#endd")
-				env_list_mode = elsed_mode
-			if iset_list_mode != 0:
-				iset_list.append("#endd")
-				iset_list_mode = elsed_mode
-			if iexec_list_mode != 0:
-				iexec_list.append("#endd")
-				iexec_list_mode = elsed_mode
 
 		elif i != "" and i != "}" and not i.startswith("#"):
 			if "#" in i:
@@ -183,8 +212,19 @@ for i in file_list:
 		_str = i.strip(" \t")
 		if _str == "};":
 			file_mode = "1"
+			script_list.append("}")
 		else:
-			iexec_list.append(i)
+			
+			if script_list_mode != elsed_mode and elsed_mode != 0:
+				for g in range(-1 - script_list_mode, -1 - len(ifd_list), -1):
+					script_list.append("%" + ifd_list[g])
+					if ifd_list[g].startswith("#ifd"):
+						script_list_mode_1 = script_list_mode_1 + 1
+				script_list_mode = elsed_mode
+			
+			script_list.append(i)
+			
+			
 
 	file_int = file_int + i_int
 
@@ -250,7 +290,10 @@ for i in iset_list:
 new_file.write("\n")
 
 for i in iexec_list:
-	if i.startswith("%"):
+	if i.startswith("%#"):
+		new_file.write("%s\n" %(i.lstrip("%")))
+
+	elif i.startswith("%"):
 		_func = i.lstrip("%")
 		_func_prefix = _service
 		if _func_prefix.endswith("*"):
@@ -281,15 +324,19 @@ for i in service_and_daemon_list:
 new_file.write("}\n\n")
 
 _mode = "0"
-for i in iexec_list:
+for i in script_list:
 	if i in service_and_daemon_list:
 		if _mode == "2":
-			new_file.write("}\n\n")
+			new_file.write("\n")
 		_service = i
 		_mode = "1"
+	
+	elif i.startswith("%#"):
+		new_file.write("%s\n" %(i.lstrip("%")))
+	
 	elif i.startswith("%"):
 		if _mode == "2":
-			new_file.write("}\n\n")
+			new_file.write("\n")
 
 		_func_prefix = _service
 		if _func_prefix.endswith("*"):
@@ -299,13 +346,10 @@ for i in iexec_list:
 		_mode = "2"
 	elif i.startswith("&"):
 		if _mode == "2":
-			new_file.write("}\n\n")
+			new_file.write("\n")
 		_mode = "0"
 	else:
 		new_file.write(i + "\n")
-
-if _mode != "0":
-	new_file.write("}\n\n")
 
 new_file.close()
 print scriptname.rstrip("i") + "s" + " is ready.\n"
