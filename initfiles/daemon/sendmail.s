0b30622ad8cb11dbe495e338e3786ee1383faa81
Thomas Kear <qupada@initng.org>
2007-03-30 00:18:53 +0000

    Haha! i got revision 5000

--- initfiles/daemon/sendmail.s
+++ initfiles/daemon/sendmail.s
@@ -21,14 +21,14 @@ setup()
 
 prepare_start()
 {
-		if [ -x /usr/bin/make -a -f /etc/mail/Makefile ]
-		then
-			@make@ all -C /etc/mail -s >/dev/null
-		else
-			for i in virtusertable access domaintable mailertable
-			do
-				[ -f /etc/mail/${i} ] && @makemap@ hash /etc/mail/${i} </etc/mail/${i}
-			done
-		fi
-		@/usr/bin/newaliases@ >/dev/null 2>&1
+	if [ -x /usr/bin/make -a -f /etc/mail/Makefile ]
+	then
+		@make@ all -C /etc/mail -s >/dev/null
+	else
+		for i in virtusertable access domaintable mailertable
+		do
+			[ -f /etc/mail/${i} ] && @makemap@ hash /etc/mail/${i} </etc/mail/${i}
+		done
+	fi
+	@/usr/bin/newaliases@ >/dev/null 2>&1
 }
