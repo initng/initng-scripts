9ec7cb63b437e46f3d019d26246efb38f07292a8
Jens Persson <xerxes2@gmail.com>
2007-04-13 00:58:50 +0000

    fixed net.s

--- initfiles/net/net.s
+++ initfiles/net/net.s
@@ -5,92 +5,85 @@
 setup()
 {
 #ifd debian linspire pingwinek ubuntu
-	iregister -s "net/all" service
-#endd
-	iregister -s "net/lo" service
-	iregister -s "net/*" service
-
-#ifd debian linspire pingwinek ubuntu
-	iset -s "net/all" stdall = "/dev/null"
-	iset -s "net/all" need = "system/bootmisc"
-	iset -s "net/all" use = "system/modules system/coldplug"
+	if [ "$SERVICE" = "net/all" ]
+    then
+	ireg service
+	iset stdall = "/dev/null"
+	iset need = system/bootmisc
+	iset use = system/modules system/coldplug
 #ifd linspire
-	iset -s "net/all" need = "system/los-networking"
+	iset need = system/los-networking
 #elsed ubuntu
-	iset -s "net/all" use = "system/ifupdown-debian daemon/cardmgr"
+	iset use = system/ifupdown-debian daemon/cardmgr
 #elsed debian linspire
-	iset -s "net/all" need = "system/ifupdown-debian"
-	iset -s "net/all" use = "daemon/cardmgr"
+	iset need = system/ifupdown-debian
+	iset use = daemon/cardmgr
 #endd
-	iset -s "net/all" provide = "virtual/net"
-#endd
-	iset -s "net/lo" need = "system/bootmisc"
-	iset -s "net/lo" provide = "virtual/net/lo"
-	iset -s "net/lo" use = "system/modules system/coldplug"
-#ifd ubuntu
-	iset -s "net/lo" use = "system/ifupdown-debian"
-#elsed debian linspire
-	iset -s "net/lo" need = "system/ifupdown-debian"
-#endd
-#ifd gentoo
-	iset -s "net/lo" exec start = "${INITNG_PLUGIN_DIR}/scripts/net/interface ${NAME} start"
-	iset -s "net/lo" exec stop = "${INITNG_PLUGIN_DIR}/scripts/net/interface ${NAME} stop"
-#elsed arch
-	iset -s "net/lo" exec start = "@/sbin/ifconfig@ lo up"
-	iset -s "net/lo" exec stop = "@/sbin/ifconfig@ lo down"
-#elsed enlisy
-	iset -s "net/lo" exec start = "/sbin/enlisy-net lo start"
-	iset -s "net/lo" exec stop = "/sbin/enlisy-net lo stop"
-#elsed
-	iset -s "net/lo" exec start = "@/sbin/ifup@ lo"
-	iset -s "net/lo" exec stop = "@/sbin/ifdown@ lo"
-#endd
-	iset -s "net/*" stdall = "/dev/null"
-	iset -s "net/*" need = "system/bootmisc"
-#ifd ubuntu
-	iset -s "net/*" use = "system/ifupdown-debian"
-#elsed debian linspire
-	iset -s "net/*" need = "system/ifupdown-debian"
-#endd
-	iset -s "net/*" use = "system/modules system/coldplug daemon/cardmgr"
-	iset -s "net/*" provide = "virtual/net virtual/net/${NAME}"
-#ifd gentoo
-	iset -s "net/*" exec start = "${INITNG_PLUGIN_DIR}/scripts/net/interface ${NAME} start"
-	iset -s "net/*" exec stop = "${INITNG_PLUGIN_DIR}/scripts/net/interface ${NAME} stop"
-#elsed arch
-	iset -s "net/*" exec start = "/sbin/ifconfig ${NAME} up"
-	iset -s "net/*" exec stop = "/sbin/ifconfig ${NAME} down"
-#elsed enlisy
-	iset -s "net/*" exec start = "/sbin/enlisy-net ${NAME} start"
-	iset -s "net/*" exec stop = "/sbin/enlisy-net ${NAME} stop"
+	iset provide = virtual/net
+	iexec start = all_start
+	iexec stop = all_stop
+	idone
+	fi
 #endd
 
-#ifd debian linspire pingwinek ubuntu
-#ifd debian linspire ubuntu
-	iexec -s "net/all" start = all_start
-	iexec -s "net/all" stop = all_stop
-#elsed pingwinek
-	iexec -s "net/all" start = all_start
-	iexec -s "net/all" stop = all_stop
-#endd
+	if [ "$SERVICE" = "net/lo" ]
+    then
+	ireg service
+	iset need = system/bootmisc
+	iset provide = virtual/net/lo
+	iset use = system/modules system/coldplug
+#ifd ubuntu
+	iset use = system/ifupdown-debian
+#elsed debian linspire
+	iset need = system/ifupdown-debian
 #endd
 #ifd gentoo
+	iset exec start = "${INITNG_PLUGIN_DIR}/scripts/net/interface ${NAME} start"
+	iset exec stop = "${INITNG_PLUGIN_DIR}/scripts/net/interface ${NAME} stop"
 #elsed arch
+	iset exec start = "@/sbin/ifconfig@ lo up"
+	iset exec stop = "@/sbin/ifconfig@ lo down"
 #elsed enlisy
+	iset exec start = "/sbin/enlisy-net lo start"
+	iset exec stop = "/sbin/enlisy-net lo stop"
 #elsed
-	iexec -s "net/*" start = net_any_start
-	iexec -s "net/*" stop = net_any_stop
+	iset exec start = "@/sbin/ifup@ lo"
+	iset exec stop = "@/sbin/ifdown@ lo"
 #endd
+	idone
+	fi
 
-#ifd debian linspire pingwinek ubuntu
-	idone -s "net/all"
+	if [ "$SERVICE" = "net/*" ]
+    then
+	ireg service
+	iset stdall = "/dev/null"
+	iset need = system/bootmisc
+#ifd ubuntu
+	iset use = system/ifupdown-debian
+#elsed debian linspire
+	iset need = system/ifupdown-debian
 #endd
-	idone -s "net/lo"
-	idone -s "net/*"
+	iset use = system/modules system/coldplug daemon/cardmgr
+	iset provide = virtual/net virtual/net/${NAME}
+#ifd gentoo
+	iset exec start = "${INITNG_PLUGIN_DIR}/scripts/net/interface ${NAME} start"
+	iset exec stop = "${INITNG_PLUGIN_DIR}/scripts/net/interface ${NAME} stop"
+#elsed arch
+	iset exec start = "/sbin/ifconfig ${NAME} up"
+	iset exec stop = "/sbin/ifconfig ${NAME} down"
+#elsed enlisy
+	iset exec start = "/sbin/enlisy-net ${NAME} start"
+	iset exec stop = "/sbin/enlisy-net ${NAME} stop"
+#elsed
+	iexec start = net_any_start
+	iexec stop = net_any_stop
+#endd
+	idone
+	fi
 }
+
 #ifd debian linspire pingwinek ubuntu
 #ifd debian linspire ubuntu
-
 all_start()
 {
                 mkdir -p /var/run/network
