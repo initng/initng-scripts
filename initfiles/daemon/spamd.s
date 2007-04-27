642228b734260fae7294a7cdf7e0d51811cded1e
Ismael Luceno <ismael@initng.org>
2007-04-27 00:46:16 +0000

    Improved spamd a bit

--- initfiles/daemon/spamd.s
+++ initfiles/daemon/spamd.s
@@ -1,12 +1,18 @@
-# NAME:
-# DESCRIPTION:
+# NAME: spamassassin
+# DESCRIPTION: Spam filter
 # WWW:
+
+#ifd debian
+[ -f /etc/default/spamassassin ] && . /etc/default/spamassassin
+#else
+OPTIONS="-c"
+#endd
 
 setup()
 {
 	ireg daemon daemon/spamd && {
 		iset need = system/bootmisc virtual/net
 		iset stdall = /dev/null
-		iset exec daemon = "@/usr/bin/spamd@ -c"
+		iset exec daemon = "@/usr/sbin/spamd:/usr/bin/spamd@ ${OPTIONS}"
 	}
 }
