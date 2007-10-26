5a9dd8db71e984e02b40be9829bf95cb546f2a3a
Ismael Luceno <ismael@initng.org>
2007-10-26 04:05:28 +0000

    daemon/squid: avoid forking

--- initfiles/daemon/squid.s
+++ initfiles/daemon/squid.s
@@ -3,14 +3,10 @@
 # DESCRIPTION: Caching web proxy server
 # WWW: http://www.squid-cache.org/
 
-setup()
-{
+setup() {
 	iregister daemon
 		iset need = system/bootmisc virtual/net
-		iset pid_file = "/var/run/squid.pid"
 		iset respawn
-		iset forks
-		iset exec daemon = "@/usr/sbin/squid@"
-		iset exec kill = "@/usr/sbin/squid@ -k shutdown"
+		iset exec daemon = "@/usr/sbin/squid@ -N"
 	idone
 }
