8d187fa0baf83015ff90af356d1c476b3fb0acae
Ismael Luceno <ismael@initng.org>
2008-02-24 21:48:54 +0000

    daemon/sshd: avoid forking

--- initfiles/daemon/sshd/this.s
+++ initfiles/daemon/sshd/this.s
@@ -5,14 +5,11 @@
 
 setup()
 {
-
 	iregister daemon
 		iset need = system/bootmisc virtual/net
 		iset conflict = daemon/dropbear
 		iset use = daemon/sshd/generate_keys
-		iset pid_file = "/var/run/sshd.pid"
-		iset forks
 		iset daemon_stops_badly
-		iexec daemon = "@/usr/sbin/sshd@"
+		iexec daemon = "@/usr/sbin/sshd@ -D"
 	idone
 }
