3885ba4cd23eb54e6a8c957e77efe7c7554afc5d
Ismael Luceno <ismael@initng.org>
2008-02-26 03:55:10 +0000

    exim/queuerunner: avoid forking

--- initfiles/daemon/exim/queuerunner.s
+++ initfiles/daemon/exim/queuerunner.s
@@ -4,7 +4,6 @@
 # WWW:
 
 QUEUEINTERVAL="30m"
-QRPIDFILE="/var/run/exim4/eximqr.pid"
 #ifd debian
 [ -f /etc/defaults/exim4 ] && . /etc/defaults/exim4
 #elsed gentoo
@@ -16,8 +15,6 @@ setup()
 	iregister daemon
 		iset need = system/bootmisc virtual/net daemon/exim/updateconf
 		iset conflict = daemon/exim/combined
-		iset pid_file = "${QRPIDFILE}"
-		iset forks
-		iexec daemon = "@/usr/sbin/exim4@ -oP ${QRPIDFILE} -q${QFLAGS}${QUEUEINTERVAL} ${COMMONOPTIONS} ${QUEUERUNNEROPTIONS}"
+		iexec daemon = "@/usr/sbin/exim4@ -q${QFLAGS}${QUEUEINTERVAL} ${COMMONOPTIONS} ${QUEUERUNNEROPTIONS}"
 	idone
 }
