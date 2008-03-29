6c69a25f1ee6f202a345601b04c25ccc5a02167a
Ismael Luceno <ismael@initng.org>
2008-03-29 07:52:45 +0000

    vdradmind: avoid forking

--- initfiles/daemon/vdradmind.s
+++ initfiles/daemon/vdradmind.s
@@ -3,15 +3,11 @@
 # DESCRIPTION:
 # WWW:
 
-setup()
-{
+setup() {
 	iregister daemon
 		iset need = system/bootmisc
 		iset use = daemon/vdr daemon/svdrpd
-		iset pid_file = "/var/run/vdradmind.pid"
 		iset stdout = /dev/null
-		iset forks
-		iset exec daemon = "@vdradmind.pl@"
-		iset exec kill = "@vdradmind.pl@ --kill"
+		iset exec daemon = "@vdradmind.pl@ --nofork"
 	idone
 }
