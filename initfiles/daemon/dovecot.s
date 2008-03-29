c70b9855c4503b882d885321850d429e5927ce35
Ismael Luceno <ismael@initng.org>
2008-03-29 07:51:39 +0000

    dovecot: avoid forking

--- initfiles/daemon/dovecot.s
+++ initfiles/daemon/dovecot.s
@@ -3,12 +3,9 @@
 # DESCRIPTION:
 # WWW:
 
-setup()
-{
+setup() {
 	iregister daemon
 		iset need = system/bootmisc virtual/net
-		iset pid_file = "/var/run/dovecot/master.pid"
-		iset forks
-		iset exec daemon = "@/usr/sbin/dovecot@"
+		iset exec daemon = "@/usr/sbin/dovecot@ -F"
 	idone
 }
