4c94d24cd2e6eea6c6ba844653e3fcd4c34f60ca
Ismael Luceno <ismael@initng.org>
2008-03-29 07:52:30 +0000

    udhcpc: avoid forking

--- initfiles/daemon/udhcpc/any.s
+++ initfiles/daemon/udhcpc/any.s
@@ -5,23 +5,21 @@
 
 [ -f /etc/conf.d/net ] && . /etc/conf.d/net
 
-setup()
-{
+setup() {
 	iregister daemon
 		iset need = system/bootmisc
 		iset use = system/modules system/coldplug
-		iset pid_file = "/var/run/udhcpc-${NAME}.pid"
 		iset respawn
-		iset forks
 		iexec daemon
 	idone
 }
 
-daemon()
-{
+daemon() {
 	eval opts=\"\$\{udhcpc_${NAME}\}\"
 	h=`hostname`
 	[ -n "${h}" -a ! "${h}" = "(none)" -a ! "${h}" = "localhost" ] &&
 		echo "${opts}" | @grep@ -w -- '-h' || opts="${opts} --hostname=${h}"
-	exec @udhcpc@ ${opts} --now --script=${INITNG_PLUGIN_DIR}/scripts/net/udhcpc-wrapper --pidfile=/var/run/udhcpc-${NAME}.pid --interface=${NAME}
+	exec @udhcpc@ ${opts} --now \
+		--script=${INITNG_PLUGIN_DIR}/scripts/net/udhcpc-wrapper \
+		--foreground --interface=${NAME}
 }
