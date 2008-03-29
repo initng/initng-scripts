7286f107a35113dd6ed3bcef785939289422948b
Ismael Luceno <ismael@initng.org>
2008-03-29 07:50:54 +0000

    ddclient: force pidfile location

--- initfiles/daemon/ddclient.s
+++ initfiles/daemon/ddclient.s
@@ -3,15 +3,17 @@
 # DESCRIPTION: Update client for DynDNS.com's dynamic IP service (and others)
 # WWW: http://ddclient.sourceforge.net/
 
+PIDFILE="/var/run/ddclient/ddclient.pid"
+
 setup()
 {
 	iregister daemon
 		iset suid = ddclient
 		iset sgid = ddclient
 		iset need = virtual/net system/bootmisc
-		iset pid_file = "/var/run/ddclient/ddclient.pid"
+		iset pid_file = "$PIDFILE"
 		iset forks
 		iset respawn
-		iset exec daemon = "@/usr/sbin/ddclient@"
+		iset exec daemon = "@/usr/sbin/ddclient@ -pid $PIDFILE"
 	idone
 }
