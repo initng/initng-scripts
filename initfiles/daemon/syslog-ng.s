b2d37755c59d5e8a3f3f4a2b09ff594366800dbc
Ismael Luceno <ismael@initng.org>
2008-03-29 07:52:11 +0000

    syslog-ng: avoid forking

--- initfiles/daemon/syslog-ng.s
+++ initfiles/daemon/syslog-ng.s
@@ -3,16 +3,13 @@
 # DESCRIPTION:
 # WWW:
 
-setup()
-{
+setup() {
 	iregister daemon
 		iset provide = virtual/syslog
 		iset need = system/bootmisc
 #ifd debian
 		iset need = daemon/syslogd/prepare
 #endd
-		iset pid_file = "/var/run/syslog-ng.pid"
-		iset forks
-		iset exec daemon = "@/sbin/syslog-ng@ -p /var/run/syslog-ng.pid"
+		iset exec daemon = "@/sbin/syslog-ng@ --foreground"
 	idone
 }
