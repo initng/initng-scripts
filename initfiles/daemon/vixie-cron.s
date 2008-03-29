11877c1605e47004d92813b82a38d7cc6b8a1ea0
Ismael Luceno <ismael@initng.org>
2008-03-29 07:52:59 +0000

    vixie-cron: avoid forking

--- initfiles/daemon/vixie-cron.s
+++ initfiles/daemon/vixie-cron.s
@@ -8,12 +8,6 @@ setup()
 	iregister daemon
 		iset need = system/bootmisc
 		iset provide = virtual/cron
-#ifd gentoo
-		iset pid_file = "/var/run/cron.pid"
-#elsed
-		iset pid_file = "/var/run/crond.pid"
-#endd
-		iset forks
-		iset exec daemon = "@/usr/sbin/cron@"
+		iset exec daemon = "@/usr/sbin/cron@ -f"
 	idone
 }
