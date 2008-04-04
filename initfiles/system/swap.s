e13a6b769e87bab0b1624546fe5109a4296d051b
Ismael Luceno <ismael@initng.org>
2008-04-04 06:24:52 +0000

    system/swap: do not fail when starting

--- initfiles/system/swap.s
+++ initfiles/system/swap.s
@@ -6,6 +6,7 @@
 setup() {
 	iregister service
 		iset need = system/initial system/mountfs/essential
+		iset start_fail_ok;
 		iset exec start = "@/sbin/swapon@ -a"
 		iset exec stop = "@/sbin/swapoff@ -a"
 	idone
