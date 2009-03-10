a1275e1113e01eeec618da62a2f5e29bf9dd2f35
Ismael Luceno <ismael@initng.org>
2009-03-10 14:09:34 +0000

    fix default path to dhcdbd (closes #779)

--- initfiles/daemon/dhcdbd.s
+++ initfiles/daemon/dhcdbd.s
@@ -1,11 +1,11 @@
 # SERVICE: daemon/dhcdbd
-# NAME:
-# DESCRIPTION:
+# NAME: dhcdbd
+# DESCRIPTION: D-Bus interface to dhclient.
 # WWW:
 
 setup() {
 	iregister daemon
 		iset need = system/bootmisc daemon/dbus
-		iset exec daemon = "@/sbin/dhcdbd@ --system --no-daemon"
+		iset exec daemon = "@/usr/sbin/dhcdbd@ --system --no-daemon"
 	idone
 }
