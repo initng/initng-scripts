af01517d67163f927b152e3a691ec6d29f2be2f2
Ismael Luceno <ismael@initng.org>
2009-04-13 13:24:04 +0000

    daemon/lighttpd: fix config path for gentoo (closes #825)

--- initfiles/daemon/lighttpd.s
+++ initfiles/daemon/lighttpd.s
@@ -7,10 +7,6 @@ setup() {
 	iregister daemon
 		iset need = system/bootmisc virtual/net
 		iset use = system/modules system/coldplug
-#ifd gentoo
-		iset exec daemon = "@/usr/sbin/lighttpd@ -D -f /etc/lighttpd.conf"
-#elsed
 		iset exec daemon = "@/usr/sbin/lighttpd@ -D -f /etc/lighttpd/lighttpd.conf"
-#endd
 	idone
 }
